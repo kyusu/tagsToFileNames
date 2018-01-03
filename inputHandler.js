e16e9bd4891699bed5a548fd466b7be4ffa618fb
Kyusu <feedmewithspam@web.de>
2018-01-03 11:25:29 +0100

    Restore filter functionality

--- inputHandler.js
+++ inputHandler.js
@@ -1,20 +1,36 @@
 const program = require('commander');
 const ll = require('lazylines');
 const R = require('ramda');
-const helpers = require('./helpers.js');
+const {changeFileTags, fileSatisfiesFilter} = require('./helpers.js');
 
-const handleInput = changeFileFunc => {
+const changeTagsOnInput = changeFileFunc => (process, tags, fileName) => {
+    const message = changeFileTags(R.partial(changeFileFunc, [tags]), fileName);
+    console.log(message);
+};
+
+const filterOnInput = (process, tags, fileName) => {
+    if (fileSatisfiesFilter(tags, fileName)) {
+        process.stdout.write(`${fileName}\n`);
+    }
+};
+
+const onLine = (process, givenTags, onInputFunc) => line => {
+    const fileName = ll.chomp(line);
+    onInputFunc(process, givenTags, fileName);
+};
+
+const handleInput = onInputFunc => {
     program.parse(process.argv);
-    const tagsToBeAdded = program.args[0].split(',').map(tag => tag.trim());
-
+    const givenTags = program.args[0].split(',').map(tag => tag.trim());
     process.stdin.resume();
     process.stdin.setEncoding('utf8');
     const inp = new ll.LineReadStream(process.stdin);
-    inp.on('line', line => {
-        const fileName = ll.chomp(line);
-        const message = helpers.changeFileTags(R.partial(changeFileFunc, [tagsToBeAdded]), fileName);
-        console.log(message);
-    });
+    inp.on('line', onLine(process, givenTags, onInputFunc));
 };
 
-module.exports = handleInput;
+const changeTags = changeFileFunc => handleInput(changeTagsOnInput(changeFileFunc));
+
+const filterTags = () => handleInput(filterOnInput);
+
+
+module.exports = {changeTags, filterTags};
